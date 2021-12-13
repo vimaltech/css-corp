620f1dd1e05bf8621c9e944c0c5e070cc3866d04
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-12-13 10:04:19 +0530

    complted toggleComplete

--- src/Pages/Todo/index.js
+++ src/Pages/Todo/index.js
@@ -1,41 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import './todoStyle.css';
 
 export default class Todo extends Component {
   state = {
-    todoText: '',
     todoList: [],
   };
 
-  onChangeText = (event) => {
-    const text = event.target.value;
-    this.setState({
-      todoText: text,
-    });
-  };
+  inputRef = createRef();
 
   addTodo = (event) => {
     event.preventDefault();
-    this.setState(({ todoList, todoText }) => ({
-      todoList: [...todoList, todoText],
-      todoText: '',
+    // const todoText = document.getElementById('todoText').value;
+    const todoText = this.inputRef.current.value;
+    this.setState(
+      ({ todoList }) => ({
+        todoList: [
+          ...todoList,
+          { id: new Date().valueOf(), text: todoText, isDone: false },
+        ],
+      }),
+      () => {
+        // document.getElementById('todoText').value = '';
+        this.inputRef.current.value = '';
+      },
+    );
+  };
+
+  toggleComplete = (item) => {
+    console.log(item);
+
+    this.setState(({ todoList }) => ({
+      todoList: todoList.map((x) => {
+        if (x.id === item.id) {
+          return { ...x, isDone: !x.isDone };
+        }
+        return x;
+      }),
     }));
   };
 
   render() {
-    const { todoText, todoList } = this.state;
+    console.log('render');
+    const { todoList } = this.state;
     return (
       <div className="container">
         <h1>Todo App</h1>
         <form onSubmit={this.addTodo}>
-          <input type="text" value={todoText} onChange={this.onChangeText} />
+          <input type="text" ref={this.inputRef} />
           <button type="submit">Add Todo</button>
         </form>
         <div className="todo-list">
           {todoList.map((item) => (
-            <div className="todo-item">
-              <input type="checkbox" name="isDone" id="isDone" />
-              <p>{item}</p>
+            <div className="todo-item" key={item.id}>
+              <input
+                type="checkbox"
+                checked={item.isDone}
+                onChange={() => this.toggleComplete(item)}
+              />
+              <p
+                style={{
+                  textDecoration: item.isDone ? 'line-through' : 'none',
+                }}
+              >
+                {item.text}
+              </p>
               <button type="button">Delete</button>
             </div>
           ))}
